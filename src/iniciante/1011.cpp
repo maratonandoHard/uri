99382213b0e14ea0053aed31cd455d6a8b5dafd1
Felipe Menino <efelipecarlos@gmail.com>
2018-02-08 21:31:41 -0200

    Conclusão do exercício 1011 e adição do 1012 até 1015

--- src/iniciante/1011.cpp
+++ src/iniciante/1011.cpp
@@ -3,6 +3,12 @@
 
 using namespace std;
 
+#define  PI 3.14159
+
 int main() {
-        
+	double raio;
+
+	cin >> raio;
+	cout << setprecision(3) << fixed;
+	cout << "VOLUME = " << ( 4.0 / 3) * PI * (raio * raio * raio) << endl;
 }
